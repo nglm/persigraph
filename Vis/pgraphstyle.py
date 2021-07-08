41fc1abd332117fabee98ac5edd940a22fef9f66
Natacha <nglm@protonmail.com>
2021-07-08 12:33:50 +0200

    gdraw in pgraphstyle

--- Vis/pgraphstyle.py
+++ Vis/pgraphstyle.py
@@ -1,5 +1,6 @@
 import numpy as np
 from typing import List, Sequence, Union, Any, Dict
+from ..utils.lists import to_iterable, to_list
 
 
 
@@ -8,89 +9,165 @@ class PGraphStyle():
 
     def __init__(
         self,
-        threshold_m: int,
-        threshold_l: int,
-        show_uniform: bool,
-        show_std: bool,
-        show_vertices: bool,
-        show_edges: bool,
-        color_list,
-        lw_min: float,
-        lw_max: float,
+        m_min: int = 0,
+        l_min: float = 0.,
+        show_uniform: bool = False,
+        show_uncertainty: bool = True,
+        show_vertices: bool = True,
+        show_edges: bool = True,
+        color_list: List = None,
+        lw_min: float = 0.5,
+        lw_max: float = 8.,
+        max_opacity: bool = False,
+        vertices = None,
+        edges = None,
+        uncertainty = None,
     ):
-        self.threshold_m = threshold_m
-        self.threshold_l = threshold_l
-
+        self.m_min = m_min
+        self.l_min = l_min
+        self.show_uniform = show_uniform
+        self.show_uncertainty = show_uncertainty
+        self.show_vertices = show_vertices
+        self.show_edges = show_edges
+        self.color_list = color_list
+        self.lw_min = lw_min
+        self.lw_max = lw_max
+        self.max_opacity = max_opacity
+        if vertices is None:
+            self.vertices = VertexStyle(
+                max_opacity = max_opacity,
+                lw_min = lw_min,
+                lw_max = lw_max,
+                color_list = color_list,
+            )
+        else:
+            self.vertices = vertices
+        if edges is None:
+            self.edges = edges(
+                max_opacity = max_opacity,
+                lw_min = lw_min,
+                lw_max = lw_max,
+                color_list = color_list,
+            )
+        else:
+            self.edges = edges
+        if uncertainty is None:
+            self.uncertainty = uncertainty(
+                max_opacity = max_opacity,
+                lw_min = lw_min,
+                lw_max = lw_max,
+                color_list = color_list,
+            )
+        else:
+            self.uncertainty = uncertainty
 
 
     def sort_components(
         components,
-        threshold_m: int = 1,
-        threshold_l: float = 0.00,
     ):
         components = [
             c for c in components
-            if (c.nb_members > threshold_m and c.life_span > threshold_l )
+            if (c.nb_members > self.m_min and c.life_span > self.l_min )
         ]
+        # If there are still components matching the thresholds
         if components:
+
+            # Sort by life span to emphasize components that live longer
             components = sort_components_by(
                 components, criteron='life_span', descending=False
-            )[0]       # [0] because sort_components_by returns a nested list
-            if components:
-                # VERTICES
-                if isinstance(components[0], Vertex):
-                    gaussians = [
-                        c for c in components
-                        if c.info['type'] in ['gaussian','KMeans','Naive']
+            )[0]     # [0] because sort_components_by returns a nested list
+
+            # --------------------- VERTICES ---------------------------
+            if isinstance(components[0], Vertex):
+                gaussians = [
+                    c for c in components
+                    if c.info['type'] in ['gaussian','KMeans','Naive']
+                ]
+                uniforms = [
+                    c for c in components if c.info['type'] == 'uniform'
+                ]
+            # ----------------------- EDGES ----------------------------
+            elif isinstance(components[0],Edge):
+                gaussians = [
+                    c for c in components
+                    if (
+                        c.v_start.info['type'] in ['gaussian','KMeans','Naive']
+                    and
+                        c.v_end.info['type'] in ['gaussian','KMeans', 'Naive']
+                    )]
+                from_to_uniforms = [
+                    c for c in components
+                    if (c.v_start.info['type'] == 'uniform')
+                    and (c.v_end.info['type'] == 'uniform')
                     ]
-                    uniforms = [ c for c in components if c.info['type'] == 'uniform' ]
-                # EDGES
-                elif isinstance(components[0],Edge):
-                    gaussians = [
-                        c for c in components
-                        if (
-                            c.v_start.info['type'] in ['gaussian','KMeans','Naive']
-                        and
-                            c.v_end.info['type'] in ['gaussian','KMeans', 'Naive']
-                        )]
-                    from_to_uniforms = [
-                        c for c in components
-                        if (c.v_start.info['type'] == 'uniform')
-                        and (c.v_end.info['type'] == 'uniform')
-                        ]
-                    to_uniforms = [
-                        c for c in components
-                        if (c.v_start.info['type'] != 'uniform')
-                        and (c.v_end.info['type'] == 'uniform')
-                        ]
-                    from_uniforms = [
-                        c for c in components
-                        if (c.v_start.info['type'] == 'uniform')
-                        and (c.v_end.info['type'] != 'uniform')
-                        ]
-                    uniforms = [to_uniforms, from_to_uniforms, from_uniforms]
+                to_uniforms = [
+                    c for c in components
+                    if (c.v_start.info['type'] != 'uniform')
+                    and (c.v_end.info['type'] == 'uniform')
+                    ]
+                from_uniforms = [
+                    c for c in components
+                    if (c.v_start.info['type'] == 'uniform')
+                    and (c.v_end.info['type'] != 'uniform')
+                    ]
+                uniforms = [to_uniforms, from_to_uniforms, from_uniforms]
         else:
             gaussians = []
             uniforms = []
 
         return gaussians, uniforms
 
-    def gdraw(self):
+    def gdraw(self, g):
         """
         Returns an aggregation of collections
         """
-        # t in range:
+        if t is None:
+            t_range = range(g.T)
+        else:
+            t_range = to_iterable(t)
 
-        # # sort components
+        collections = []
+        for t in t_range:
 
-        # # create collections for vertices if necessary
-        # # # create collections for uniform vertices if necessary
+            # Collections for vertices if necessary
+            if self.show_vertices:
+                if not isinstance(vertices, list):
+                    vertices = to_list(g._vertices[t])
 
-        # # create collections for edges if necessary
-        # # # create collections for uniform edges if necessary
+                # Keep only vertices respecting the thresholds,
+                # and distinguish between gaussian and uniform vertices
+                gaussians, uniforms = sort_components(vertices)
 
-        # # create collections for std if necessary
+                # Collections for gaussian vertices
+                collections.append(pg_style.vertices.cdraw(g, gaussians))
 
-        # # return collections
+                # Collections for uniform vertices if necessary
+                if self.show_uniform:
+                    raise NotImplementedError('Cannot display uniform vertices')
+
+            # Collections for edges if necessary
+            if (self.show_edges or self.show_uncertainty) and (t < g.T-1):
+                if not isinstance(edges, list):
+                    edges = to_list(g._edges[t])
+
+                # Keep only edges respecting the thresholds,
+                # and distinguish between gaussian and uniform edges
+                gaussians, uniforms = sort_components(edges)
+
+                # Collections for gaussian edges
+                if self.show_edges:
+                    collections.append(pg_style.edges.cdraw(g, gaussians))
+
+                # Collections for uniform edges if necessary
+                if self.show_uniform:
+                    raise NotImplementedError('Cannot display uniform edges')
+
+                # Collections for uncertainty if necessary
+                if self.show_uncertainty:
+
+                    # Collections for uncertainty
+                    collections.append(pg_style.uncertainty.cdraw(g, gaussians))
+
+        return collections
 
 
