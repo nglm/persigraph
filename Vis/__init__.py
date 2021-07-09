1e45c9163b488ee6bac52b6699907b39db385103
Natacha <nglm@protonmail.com>
2021-07-09 16:47:52 +0200

    fixing relative import

--- Vis/__init__.py
+++ Vis/__init__.py
@@ -1,8 +1,7 @@
 # To replace 'from .vertexstyle import VertexStyle'
 # And use from Vis import VertexStyle'
-
+from .componentstyle import ComponentStyle
 from .vertexstyle import VertexStyle
 from .edgestyle import EdgeStyle
 from .uncertaintystyle import UncertaintyStyle
-from .componentstyle import ComponentStyle
 from .pgraphstyle import PGraphStyle
