34b49c74fec2e91481297f49284a7e8b82d025dd
Natacha <nglm@protonmail.com>
2021-11-08 16:34:51 +0100

    smoothing data and plot its mean

--- DataAnalysis/preprocess.py
+++ DataAnalysis/preprocess.py
@@ -65,50 +65,53 @@ def extract_from_files(
         members = smoothing_mjo(members)
     return members, time
 
-def preprocessing_mjo(filename):
+def preprocessing_mjo(filename, smooth=False):
     clear_double_space(filename)
-    return extract_from_files(filename)
+    return extract_from_files(filename, smooth=smooth)
 
-def _draw_mjo_line(start, end, steps, arc=True):
-    # start is the first element inside the very weak circle
-    # end is the last element inside the very weak circle
-    # steps is the number of time steps spent inside this cercle
-    # If there is just one step, return start
-
+def _draw_mjo_line(values, arc=True):
+    # values[0] is the first element inside the very weak circle
+    # values[-1] is the last element inside the very weak circle
+    # If there is just one step, return values[0]
+    steps = len(values)
     if steps == 1:
-        return np.array([start]).T
+        return np.array([values[0]]).T
     else:
         # Initialize the angle (note that if arc is false then phi is actually)
         # RMM2
-        phi_start = start[1]
-        phi_end = end[1]
+        phi_start = values[0][1]
+        phi_end = values[-1][1]
         if arc:
             # To polar
-            for p in [start, end]:
-                p_old = np.copy(p)
+            polar = np.zeros_like(values)
+            for p, v in zip(polar, values):
                 # radius
-                p[0] = np.sqrt(p_old[0]**2 + p_old[1]**2)
+                p[0] = np.sqrt(v[0]**2 + v[1]**2)
                 # angle
-                p[1] = (atan2(p_old[1], p_old[0])) % (2*np.pi)
+                p[1] = (atan2(v[1], v[0])) % (2*np.pi)
             # If    (end -> 2pi -> start)
-            phi_start = start[1]
-            phi_end = end[1]
-            if abs(2*np.pi+start[1] - end[1]) < abs(start[1] - end[1]):
+
+            phi_start = polar[0][1]
+            phi_end = polar[-1][1]
+            if abs(2*np.pi+phi_start -phi_end) < abs(phi_start -phi_end):
                 # start is ahead of end yes, but if it is just a little bit
-                # then go backward (2/3, 1/3 rule)
-                if abs(2*np.pi+start[1] - end[1]) < np.pi/3:
+                # then go backward (4pi/3, 2pi/3 rule)
+                if abs(2*np.pi+phi_start - phi_end) < 2*np.pi/3:
                     phi_start += 2*np.pi
             # Elif  (start -> 2pi -> end)
-            elif abs(start[1] - (2*np.pi + end[1])) < abs(start[1] - end[1]):
+            elif abs(phi_start - (2*np.pi + phi_end)) < abs(phi_start -phi_end):
                 # end if ahead of start, it's all good, just add 2pi to end
                 phi_end += 2*np.pi
             # Else: the shortest path does not cross 2pi
             else:
                 # if start is too ahead of end, do a loop (2/3, 1/3 rule)
-                if ((start[1] > end[1]) and (abs(start[1] - end[1]) > np.pi/3)):
+                if (
+                    (phi_start > phi_end)
+                    and (abs(phi_start -phi_end) > 2*np.pi/3)
+                ):
                     phi_end += 2*np.pi
         path = np.concatenate([
-            [np.linspace(start[0], end[0], num=steps, endpoint=True)],
+            [[p[0] for p in polar]],
             [np.linspace(phi_start, phi_end, num=steps, endpoint=True)]
         ], axis=0)
         if arc:
@@ -117,12 +120,12 @@ def _draw_mjo_line(start, end, steps, arc=True):
         else:
             return path
 
-def smoothing_mjo(members, cartesian=True, r=0.80):
+def smoothing_mjo(members, cartesian=True, r=0.70):
     def cond(m, t):
         if cartesian:
-            return np.sqrt(m[0, t]**2 + m[1, t]**2) <= r
+            return (np.sqrt(m[0, t]**2 + m[1, t]**2) <= r)
         else:
-            return m[0, t] <= r
+            return (m[0, t] <= r)
     # If radius < r, find the next time step with
     # a radius > r and draw an arc between them
     # To know the direction of the arc use a 1/3 - 2-3 rule since it is
@@ -131,18 +134,19 @@ def smoothing_mjo(members, cartesian=True, r=0.80):
     for m in members:
         t = 0
         while (t < T):
+            values = []
             # If we enter the very weak circle
             if cond(m, t):
                 t_start = t
                 t_end = t + 1
                 # The first element inside the very weak circle:
-                start = np.copy(m[:, t])
+                values.append(np.copy(m[:, t]))
                 while (t_end < T and cond(m, t_end)):
+                    # The last element inside the very weak circle:
+                    values.append(m[:, t_end])
                     t_end += 1
-                # The last element inside the very weak circle:
-                end = np.copy(m[:, t_end-1])
                 m[:, t_start:t_end] =_draw_mjo_line(
-                    start, end, t_end-t_start, arc=True
+                    values, arc=True
                 )
                 # update t for the next loop
                 t = t_end
@@ -160,7 +164,6 @@ def _get_2pi_k(angles):
         k[t] = k[t-1]
         # was positive and gets negative 'by the left' (+1: 4pi/3 rule)
         if (
-
             (angles[t-1] >= 0 and angles[t] < 0)
             and (abs(angles[t]+2*np.pi - angles[t-1]) <= 4*np.pi/3)
         ):
@@ -185,6 +188,7 @@ def to_polar(members):
         # transform to [0, 2pi] range
         k = _get_2pi_k(members_conv[1, :])
         members_conv[1, :] += 2*np.pi*k
+        assert np.all(members_conv[0, :]>=0)
     else:
         # Radii
         members_conv[:, 0, :] = np.sqrt(
@@ -194,8 +198,8 @@ def to_polar(members):
         members_conv[:, 1, :] = np.arctan2(members[:, 1, :], members[:, 0, :])
         for m in members_conv[:, 1, :]:
             k = _get_2pi_k(m)
-            m_copy = np.copy(m)
             m += 2*np.pi*k
+        assert np.all(members_conv[:, 0, :]>=0)
     return members_conv
 
 def to_cartesian(members):
