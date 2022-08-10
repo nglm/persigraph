063c99a816102eef3b034e2817ac54fe0125cb1b
Natacha <nglm@protonmail.com>
2022-08-10 14:20:13 +0200

    reorganise folders

--- GraphApp/mjo/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MjoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mjo'
--- GraphApp/mjo/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.main),
-    path("relevant/", views.relevant),
-]
