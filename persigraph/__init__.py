8302d915779833eb98b14b2253f674825c9dab94
Natacha <nglm@protonmail.com>
2022-08-25 12:33:22 +0200

    add vis and datasets to the package

--- persigraph/__init__.py
+++ persigraph/__init__.py
@@ -8,3 +8,7 @@
 # We do that to skip the persistent graph folder (which itself
 # skip the persistentgraph.py file and access the class directly)
 from .persistentgraph import PersistentGraph
+
+# Select which modules should be available to the user
+from . import vis
+from . import datasets
